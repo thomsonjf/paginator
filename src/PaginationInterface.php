09ed8a51b2c744e0d3a731742fa0c7b9b6dc0807
James Thomson <james.thomson@propertysoftwaregroup.com>
2019-05-21 10:43:50 +0100

    Added src folder

---
+++ src/PaginationInterface.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace ThomsonJf\Pagination;
+
+/**
+ * Interface PaginationInterface
+ * @package ThomsonJf\Pagination
+ */
+interface PaginationInterface
+{
+    /**
+     * Get a list of elements for the current page, paginated
+     * according to the perPage settings.
+     *
+     * @param int $page
+     * @return mixed
+     */
+    public function paginate(int $page): array;
+
+    /**
+     * Determine if there is another page after the current one
+     *
+     * @return bool
+     */
+    public function hasNextPage(): bool;
+
+    /**
+     * Determine if there is a previous page before the current one
+     *
+     * @return bool
+     */
+    public function hasPreviousPage(): bool;
+
+    /**
+     * Return the count of elements on the current page
+     *
+     * @return int
+     */
+    public function getCurrentPageElementsCount(): int ;
+
+    /**
+     * Return the total count of elements accross all pages
+     *
+     * @return int
+     */
+    public function getTotalElementsCount(): int;
+
+    /**
+     * Get an array list of pages
+     *
+     * @return array
+     */
+    public function getPagesList(): array;
+
+    /**
+     * Get the current per page setting
+     *
+     * @return int
+     */
+    public function getPageCount(): int;
+}
---
+++ src/Paginator/LengthAwarePaginator.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace ThomsonJf\Pagination\Paginator;
+
+use ThomsonJf\Pagination\PaginationInterface;
+
+/**
+ * Class LengthAwarePaginator
+ * @package ThomsonJf\Paginator
+ */
+class LengthAwarePaginator implements PaginationInterface
+{
+    /**
+     * The total count of elements
+     *
+     * @var int
+     */
+    protected $totalCount;
+
+    /**
+     * Number of elements per page
+     *
+     * @var int
+     */
+    protected $perPage = 10;
+
+    /**
+     * Holds the number of the current page
+     *
+     * @var int
+     */
+    protected $currentPage = 1;
+
+    /**
+     * Iterator object if instantiated with array object or iterator
+     *
+     * @var \ArrayIterator
+     */
+    protected $iterator;
+
+    /**
+     * Paginator constructor.
+     * @param mixed $elements
+     * @param int $perPage
+     */
+    public function __construct($elements, $perPage = 10)
+    {
+        $this->perPage = $perPage;
+        if (is_array($elements)) {
+            $this->iterator = new \ArrayIterator($elements);
+        } else if ($elements instanceof \ArrayObject) {
+            $this->iterator = $elements->getIterator();
+        } else if ($elements instanceof \ArrayIterator) {
+            $this->iterator = $elements;
+        } else {
+            throw new \InvalidArgumentException(
+                'Invalid type supplied to Paginator constructor - must be ArrayObject, Traversable or array'
+            );
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function paginate(int $page): array
+    {
+        // Set current page (used in other calculations)
+        $this->currentPage = $page;
+
+        // seek to offset (one based index)
+        $offset = ($page - 1) * $this->perPage;
+
+        if ($offset > 0) {
+            $this->iterator->seek($offset);
+        } else {
+            $this->iterator->rewind();
+        }
+
+        $pageElements = [];
+        $count = 0;
+        while ($this->iterator->valid() && $count < $this->perPage) {
+            $pageElements[] = $this->iterator->current();
+            $this->iterator->next();
+            ++$count;
+        }
+
+        return $pageElements;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function hasNextPage(): bool
+    {
+        return $this->getPageCount() > 0 ? $this->getPageCount() > $this->currentPage : false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function hasPreviousPage(): bool
+    {
+        return $this->getPageCount() > 1 && $this->currentPage > 1;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCurrentPageElementsCount(): int
+    {
+        if ($this->getPageCount() > 0) {
+            // It's only the last page that's likely to have less than perPage records
+            if ($this->currentPage === $this->getPageCount()) {
+                return $this->getTotalElementsCount() % $this->perPage;
+            }
+
+            return $this->perPage;
+        }
+
+        return 0;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTotalElementsCount(): int
+    {
+        if (null === $this->totalCount) {
+            $this->totalCount = iterator_count($this->iterator);
+        }
+
+       return $this->totalCount;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPagesList(): array
+    {
+        if ($this->getPageCount() > 0) {
+            return range(1, $this->getPageCount());
+        }
+
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPageCount(): int
+    {
+        if ($this->getTotalElementsCount() > 0) {
+            return ceil($this->getTotalElementsCount() / $this->perPage);
+        }
+
+        return 0;
+    }
+}
